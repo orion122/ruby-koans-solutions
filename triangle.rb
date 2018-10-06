2e7b76d0edb1dc07604023a14d27c558951c5efa
ramazan yaraliev <yaru@bk.ru>
2018-10-06 21:37:50 +0300

    complete triangle 2

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  sorted_sides = [a, b, c].sort
+
+  raise TriangleError if [a, b, c].min <= 0 || sorted_sides[0] + sorted_sides[1] <= sorted_sides[2]
+
   return :equilateral if [a, b, c].uniq.count == 1
   return :isosceles if [a, b, c].uniq.count == 2
   :scalene
